7157ebda08b73d1258aa85a381e2d1ade35aaff3
mlopez8621 <leonardo.lopez86a@gmail.com>
2024-03-10 21:08:49 -0500

    rename HolaController

--- src/main/java/com/nttdata/pruebaTecnicaSpringBoot/HolaController.java
+++ src/main/java/com/nttdata/pruebaTecnicaSpringBoot/HolaController.java
@@ -1,14 +1,13 @@
 package com.nttdata.pruebaTecnicaSpringBoot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class HolaMundoController {
+public class HolaController {
     @GetMapping("/hola")
     public Map<String, String> saludo() {
         Map<String, String> response = new HashMap<>();
