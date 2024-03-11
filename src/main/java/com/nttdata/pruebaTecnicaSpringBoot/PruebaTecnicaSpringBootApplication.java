86dc632fc7d722b3eebfc628438d6d6bc7777759
mlopez8621 <leonardo.lopez86a@gmail.com>
2024-03-10 19:13:33 -0500

    first test web service

---
+++ src/main/java/com/nttdata/pruebaTecnicaSpringBoot/PruebaTecnicaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.pruebaTecnicaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/pruebaTecnicaSpringBoot/PruebaTecnicaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.pruebaTecnicaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
